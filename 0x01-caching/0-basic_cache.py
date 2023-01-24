33966171d31b856897688af35d3a79a21576691a
joshua530 <joshuaomari5@gmail.com>
2023-01-24 09:38:42 +0300

    implement basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+contains caching class
+"""
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    contains caching class
+    """
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
