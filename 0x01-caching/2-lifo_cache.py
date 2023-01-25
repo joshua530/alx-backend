6611390cd8473ac1774576e0f823525a0c2eac53
joshua530 <joshuaomari5@gmail.com>
2023-01-25 07:47:11 +0300

    fix lifo cache

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -13,26 +13,22 @@ class LIFOCache(BaseCaching):
     """
     def __init__(self):
         """init method"""
-        self.__cache_keys = []
         super().__init__()
+        self.last_key = ''
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
-        if key is None or item is None:
-            return
-        if len(self.__cache_keys) >= BaseCaching.MAX_ITEMS:
-            if key not in self.cache_data:
-                to_remove = self.__cache_keys[len(self.__cache_keys) - 1]
-                print("DISCARD: {}".format(to_remove))
-                self.cache_data.pop(to_remove)
-                self.__cache_keys = self.__cache_keys[0: len(self.__cache_keys) - 1]
-        self.cache_data[key] = item
-        self.__cache_keys.append(key)
+        """ Assign to the dictionary, LIFO algorithm, add element """
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                print("DISCARD: {}".format(self.last_key))
+                self.cache_data.pop(self.last_key)
+            self.last_key = key
 
     def get(self, key):
-        """ Get an item by key
-        """
-        if key is None or key not in self.cache_data.keys():
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
             return None
-        return self.cache_data[key]
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            return
