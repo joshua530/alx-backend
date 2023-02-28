2873bada6c95e3ba3efd83bf0828e13d2de60929
joshua530 <joshuaomari5@gmail.com>
2023-02-28 08:00:57 +0300

    Queuing System in JS

---
+++ 0x03-queueing_system_in_js/5-subscriber.js
@@ -0,0 +1,22 @@
+const redis = require('redis');
+const client = redis.createClient();
+
+const KILL_SERVER = 'KILL_SERVER';
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (_err, msg) => {
+  console.log(msg);
+  if (msg === KILL_SERVER) {
+    client.unsubscribe();
+    client.quit();
+  }
+});
