ffbef0a735c0e1c365da0a8384db112402126cdd
joshua530 <joshuaomari5@gmail.com>
2023-04-06 22:15:03 +0300

    add helper comments

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,6 +14,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
+        # is None is right, not == None
         if key is None or item is None:
             return
         self.cache_data[key] = item
@@ -21,6 +22,7 @@ class BasicCache(BaseCaching):
     def get(self, key):
         """ Get an item by key
         """
+        # is None is right, not == None
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
